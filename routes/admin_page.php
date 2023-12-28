ce5e6eba2cdbabacfa2534a9a20d98f54383e740
Hassan-Tamer <hassantamerha@gmail.com>
2023-12-28 04:14:50 +0200

    basic sidemenu

---
+++ routes/admin_page.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomAuthController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+// Route::get('/',[CustomAuthController::class,'login']);
+// Route::get('/register',[CustomAuthController::class,'register']);
+// Route::post('/register_user',[CustomAuthController::class,'registerUser'])->name('register_user');
+// Route::post('/login_user',[CustomAuthController::class,'loginUser'])->name('login_user');
+// Route::get('/admin_profile',[CustomAuthController::class,'adminProfile']);
+// Route::get('/user_profile',[CustomAuthController::class,'userProfile']);
