6766bc552d3eff08b574bd5d500537c457206e77
yassin <yassine.tamam3@gmail.com>
2023-12-29 22:37:59 +0200

    working on rent

--- resources/views/components/customer_sidebar.blade.php
+++ resources/views/components/customer_sidebar.blade.php
@@ -32,10 +32,7 @@
           </button>
           <ul id="dropdown1" class="hidden py-2 space-y-2">
             <li>
-              <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Rent</a>
-            </li>
-            <li>
-              <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Pick up</a>
+              <a href="rent" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Rent/Pick up</a>
             </li>
             <li>
               <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Pay your trips</a>
---
+++ resources/views/customer/rent.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>rent/pickup</title>
+   <link href="{{ asset('css/admin/sidebar.css') }}" rel="stylesheet">
+   <link href="{{ asset('css/customer/rent.css') }}" rel="stylesheet">
+   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+@vite('resources/css/app.css')
+
+</head>
+<body>
+   <div>
+      @include('components.customer_sidebar')
+   </div>
+
+
+
+</body>
