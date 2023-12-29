881e79813b43ff1e6c8bbe8ffc7c64ce13d3c369
yassin <yassine.tamam3@gmail.com>
2023-12-29 21:12:27 +0200

    inshallah final customer profile

--- resources/views/customer/Profile.blade.php
+++ resources/views/customer/Profile.blade.php
@@ -5,11 +5,18 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         @vite('resources/css/app.css')
-        <title>Customer Profile</title>
         <link href="{{ asset('css/profile.css') }}" rel="stylesheet">
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <title>Login registration page</title>
+        <link rel="stylesheet" href="/css/login.css">
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     </head>
 <body>
-    <h1>Customer Profile</h1>
+    <div class = "wrapper">
+        <h1>My Profile</h1>
+
     <div>
       @include('components.customer_sidebar')
     </div>
@@ -23,6 +30,7 @@
         <div class="data_item"><p > district: {{$auth->district}} </p></div>
         <div class="data_item"><p > email: {{$auth->email}} </p></div>
         <div class="data_item"><p > phone number: {{$auth->phone_number}} </p></div>
+    </div>
     </div>
 </body>
 </html>
