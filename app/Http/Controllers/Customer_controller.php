912ce2c5aae257eb9294e2d7f7464fc2879eceb5
hanoo2002 <hana,ghanem@gmail.com>
2023-12-30 19:49:18 +0200

    rent backend

--- app/Http/Controllers/Customer_controller.php
+++ app/Http/Controllers/Customer_controller.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\admin;
 use App\Models\Customer;
 use Hash;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Session;
 use App\Models\Car;
@@ -23,11 +24,30 @@ class Customer_controller extends Controller
 
     public function rentCar($car)
     {
-        //dd($car, auth()->id());
+        dd(auth()->id());
 
         // TODO query to rent car
 
-        // Return with success message
+        $updateQuery = "UPDATE car_statuses
+        SET status = 'Booked'
+        WHERE car_statuses.car_id = :id";
+
+        DB::update($updateQuery, array_filter([
+
+            "id" => $car,
+        ]));
+
+        $column1Value = 'value1';
+        $column2Value = 'value2';
+        $column3Value = 'value3';
+        $column4Value = 'value4';
+
+        // Build the SQL query string
+        $sql = "INSERT INTO procedures (cust_id, car_id ,date, procedure) VALUES (?, ?, ?, ?)";
+
+        // Perform the insert
+        DB::insert($sql, [auth()->cust_id(), $car, Carbon::now(), "Rent"]);
+
         session()->flash("success", "Car rented successfully");
 
         return back();
@@ -70,7 +90,7 @@ class Customer_controller extends Controller
         return view("customer.rent", compact('cars'));
     }
 
-//    public function search_car(Request $request)
+    //    public function search_car(Request $request)
 //    {
 //
 //        //return view('customer.viewCars_cust', compact('results'));
