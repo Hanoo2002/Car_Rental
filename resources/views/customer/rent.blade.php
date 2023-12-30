8fafbd4ec35a5e90d525087efbdae75f39e07c22
yassin <yassine.tamam3@gmail.com>
2023-12-30 02:13:56 +0200

    minor changes

--- resources/views/customer/rent.blade.php
+++ resources/views/customer/rent.blade.php
@@ -13,11 +13,35 @@
 </head>
 
 <body>
-   <div>
-      @include('components.customer_sidebar')
-   </div>
-   <div class="main_container">
-      <div class="view_component">
+<div>
+   @include('components.customer_sidebar')
+</div>
+
+   <div class ="view">
+      <div class="wrapper1">
+         <h1>Available Cars</h1>
+
+         <div class="view_component">
+            <div class="row">
+               <div class="col-12">
+                  <table class="table table-bordered">
+                     <thead>
+                     <tr>
+                        <th>Model</th>
+                        <th>Color</th>
+                        <th>year</th>
+                     </tr>
+                     </thead>
+
+                  </table>
+               </div>
+            </div>
+         </div>
+
+
+      </div>
+      <div class="wrapper2">
+         <h1>search by</h1>
          <form action="search_user" method="GET">
             @csrf
             <input type="text" name="search" placeholder="Search users" required id="searchbar">
@@ -25,44 +49,9 @@
             <a href='original_page'><button type="button" class="reset-button">Reset</button></a>
 
          </form>
-         <div class="row">
-            <div class="col-12">
-               <table class="table table-bordered">
-                  <thead>
-                     <tr>
-                        <th>Model</th>
-                        <th>Color</th>
-                        <th>year</th>
-                     </tr>
-                  </thead>
-                  <tbody>
-                     @foreach($cars as $car)
-                     <tr>
-                        <td>{{$car['model']}}</td>
-                        <td>{{$car['color']}}</td>
-                        <td>{{$car['year']}}</td>
-                     </tr>
-                     @endforeach
-                  </tbody>
-               </table>
-            </div>
-         </div>
-      </div>
-      <div class="search_component">
 
       </div>
-
    </div>
-
-   <div class ="view">
-      <div class="wrapper1">
-         <h1>Available Cars</h1>
-      </div>
-      <div class="wrapper2">
-            <h1>search by</h1>
-      </div>
-      </div>
-
 
 
 
